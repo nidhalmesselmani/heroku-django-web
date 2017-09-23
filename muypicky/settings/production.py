32b656286848a59a2c071f78005878240f7b6411
Nidhal Messelmani <nidhalmesselmani@yahoo.fr>
2017-09-23 14:07:25 -0700

    Updated requirements.txt

--- muypicky/settings/production.py
+++ muypicky/settings/production.py
@@ -127,15 +127,15 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 
 CORS_REPLACE_HTTPS_REFERER      = True
