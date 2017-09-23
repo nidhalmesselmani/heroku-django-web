de1e1e0106c90194d900a79923ad389b0e650fc0
Nidhal Messelmani <nidhalmesselmani@yahoo.fr>
2017-09-23 15:17:55 -0700

    Initial Heroku commit

--- muypicky/settings/production.py
+++ muypicky/settings/production.py
@@ -16,6 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 
@@ -136,11 +137,11 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 
 
