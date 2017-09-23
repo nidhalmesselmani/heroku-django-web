b5eca4224ab6c2baa634e6e5fa267634d3857f36
Nidhal Messelmani <nidhalmesselmani@yahoo.fr>
2017-09-23 15:05:44 -0700

    Initial Heroku commit

--- muypicky/settings/production.py
+++ muypicky/settings/production.py
@@ -137,7 +137,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATIC_ROOT= os.path.join(PROJECT_DIR,'static')
+STATIC_ROOT= os.path.join(BASE_DIR,'static_root')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = ()
 
