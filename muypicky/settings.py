42a685340bda68207a85c6b4b700b2268921f207
Nidhal Messelmani <nidhalmesselmani@yahoo.fr>
2017-09-23 15:41:16 -0700

    Initial Heroku commit

--- muypicky/settings.py
+++ muypicky/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
@@ -130,4 +130,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
