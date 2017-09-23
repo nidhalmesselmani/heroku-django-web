027195cd03bd1af941b0cd33e5d82c66d653cb5c
Nidhal Messelmani <nidhalmesselmani@yahoo.fr>
2017-09-23 16:45:06 -0700

    Initial Heroku commit

--- muypicky/urls.py
+++ muypicky/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from .views import hello_world
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$/', hello_world),
+]
 ]
